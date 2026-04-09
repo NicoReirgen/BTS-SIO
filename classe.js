0de7e0453811065c27a8902f27cbb24db36da860
Nicolas NEGRIER <33528687+NicoReirgen@users.noreply.github.com>
2026-04-09 22:50:40 +0000

    add

---
+++ classe.js
@@ -0,0 +1,7 @@
+export const classe = [
+    { nom: " Martin ", notes: [12, 15, 9] },
+    { nom: "Durand", notes: "14,10,16" },
+    { nom: "Dupont", notes: [10, "13", 15] },
+    { nom: "", notes: [8, 9, 7] },
+    { nom: "Leroy", notes: [] }
+];
