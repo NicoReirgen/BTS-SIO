1bcb923cfd9b629febff5ddd4b8568c454886204
Nicolas NEGRIER <33528687+NicoReirgen@users.noreply.github.com>
2026-04-09 23:19:43 +0000

    update

--- classe.js
+++ classe.js
@@ -1,7 +1,44 @@
 export const classe = [
-    { nom: " Martin ", notes: [12, 15, 9] },
-    { nom: "Durand", notes: "14,10,16" },
-    { nom: "Dupont", notes: [10, "13", 15] },
-    { nom: "", notes: [8, 9, 7] },
-    { nom: "Leroy", notes: [] }
+    {
+        nom: " Martin ",
+        notes: [12, 15, 9],
+        retards: 1,
+        rattrapagesValides: false
+    },
+    {
+        nom: "Durand",
+        notes: "14,10,16",
+        retards: 0,
+        rattrapagesValides: false
+    },
+    {
+        nom: "Dupont",
+        notes: [10, "13", 15],
+        retards: 2,
+        rattrapagesValides: false
+    },
+    {
+        nom: "",
+        notes: [8, 9, 7],
+        retards: 1,
+        rattrapagesValides: true
+    },
+    {
+        nom: "Leroy",
+        notes: [],
+        retards: 0,
+        rattrapagesValides: false
+    },
+    {
+        nom: " Bernard",
+        notes: [7, 8, 9],
+        retards: 1,
+        rattrapagesValides: true
+    },
+    {
+        nom: "Petit",
+        notes: [6, 7, "abs"],
+        retards: 5,
+        rattrapagesValides: true
+    }
 ];
